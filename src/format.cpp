17c0e38f7c1ee4f1940c0e769cc8f5f0a354756b
Andres Campos <andres.campos.9841@gmail.com>
2021-10-06 21:58:42 -0700

    fixed bug in format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -1,6 +1,7 @@
 #include <string>
-
+#include <iomanip>
 #include "format.h"
+
 
 using std::string;
 using std::to_string;
@@ -14,6 +15,16 @@ string Format::ElapsedTime(long seconds) {
     MM = seconds/60;
     HH = MM/60;
 
-    return std::to_string(HH)+":"+std::to_string(MM%60)+":"+std::to_string(seconds%60); 
+    std::ostringstream stream;
     
+    string hour, min, sec;
+    hour = std::to_string(HH);
+    min = std::to_string(MM%60);
+    sec = std::to_string(seconds%60); 
+    
+    stream << std::setw(2) << std::setfill('0') << hour << ":" 
+    << std::setw(2) << std::setfill('0') << min << ":"
+    << std::setw(2) << std::setfill('0') << sec;
+
+    return stream.str();
 }
